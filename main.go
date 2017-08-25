65f30f4c8494e2b704af673e4b4ba5e9a90e7c81
Philippe Hässig <phil@nine.ch>
2017-08-25 09:58:39 +0200

    Finish requests before shutdown

--- main.go
+++ main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/ninech/frau-schultz/api"
 )
 
+// DefaultPort defines the app's default HTTP port
+const DefaultPort = "8080"
+
+var srv *http.Server
+
 func main() {
 	fmt.Println("Hallo, ich bin Frau Schultz!")
+	serveRequests()
+	waitForQuitSignal()
+	log.Println("Frau Schultz sagt auf Wiedersehen ...")
+	gracefullyStop(5 * time.Second)
+}
 
-	engine := api.GetMainEngine()
-	engine.Run(":8080")
+func serveRequests() {
+	router := api.GetMainEngine()
+
+	srv = &http.Server{
+		Addr:    ":" + DefaultPort,
+		Handler: router,
+	}
+
+	go func() {
+		// service connections
+		if err := srv.ListenAndServe(); err != nil {
+			log.Printf("listen: %s\n", err)
+		}
+	}()
+}
+
+func waitForQuitSignal() {
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
+
+func gracefullyStop(timeout time.Duration) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
 }
