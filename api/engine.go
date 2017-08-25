39636208c28a7b41a294dd513657ef8e0950c3c5
Philippe Hässig <phil@nine.ch>
2017-08-25 10:31:02 +0200

    Add dummy event handler endpoint

--- api/engine.go
+++ api/engine.go
@@ -7,12 +7,17 @@ func GetMainEngine() *gin.Engine {
 	r := gin.Default()
 
 	v1 := r.Group("/v1")
-
-	v1.GET("/health", endpointHealth)
-
+	{
+		v1.GET("/health", endpointHealth)
+		v1.POST("/event", endpointEvent)
+	}
 	return r
 }
 
 func endpointHealth(c *gin.Context) {
 	c.JSON(200, gin.H{"message": "pong"})
+}
+
+func endpointEvent(c *gin.Context) {
+	c.JSON(200, gin.H{"message": "Event received. Thank you."})
 }
--- api/engine_test.go
+++ api/engine_test.go
@@ -1,21 +1,24 @@
 package api_test
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"github.com/ninech/actuator/api"
 )
 
 func ServeRequest(method string, endpoint string) *httptest.ResponseRecorder {
 	server := api.GetMainEngine()
-	recorder := httptest.NewRecorder()
+	response := httptest.NewRecorder()
 	request, _ := http.NewRequest(method, endpoint, nil)
 
-	server.ServeHTTP(recorder, request)
+	server.ServeHTTP(response, request)
 
-	return recorder
+	return response
 }
 
 func TestUnknownRoute(t *testing.T) {
@@ -29,7 +32,19 @@ func TestUnknownRoute(t *testing.T) {
 func TestEndpointHealth(t *testing.T) {
 	recorder := ServeRequest("GET", "/v1/health")
 
-	if recorder.Code != http.StatusOK {
-		t.Errorf("Status code should be %v, was %d. Location: %s", http.StatusOK, recorder.Code, recorder.HeaderMap.Get("Location"))
-	}
+	assert.Equal(t, recorder.Code, http.StatusOK, "they should be equal")
+}
+
+func TestEndpointEvent(t *testing.T) {
+	response := ServeRequest("POST", "/v1/event")
+
+	assert.Equal(t, response.Code, http.StatusOK, "they should be equal")
+}
+
+func TestEndpointEventBody(t *testing.T) {
+	response := ServeRequest("POST", "/v1/event")
+
+	body, _ := ioutil.ReadAll(response.Body)
+
+	assert.JSONEq(t, `{"message":"Event received. Thank you."}`, string(body), "should be equal")
 }
