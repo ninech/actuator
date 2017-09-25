5638ecedcbd5bb080e981b03016b9c4732aa720b
Philippe Hässig <phil@nine.ch>
2017-09-25 08:52:31 +0200

    Run handler in the background

--- test/mock_event_handler.go
+++ test/mock_event_handler.go
@@ -7,13 +7,15 @@ import (
 
 func NewMockEventHandler(message string) *MockEventHandler {
 	response := actuator.EventResponse{Message: message}
-	return &MockEventHandler{EventResponse: response}
+	done := make(chan bool, 1)
+	return &MockEventHandler{EventResponse: response, Done: done}
 }
 
 type MockEventHandler struct {
 	EventResponse   actuator.EventResponse
 	EventWasHandled bool
 	LastEvent       *github.Event
+	Done            chan bool
 }
 
 func (h *MockEventHandler) GetEventResponse(event *github.Event) *actuator.EventResponse {
@@ -24,4 +26,5 @@ func (h *MockEventHandler) GetEventResponse(event *github.Event) *actuator.Event
 func (h *MockEventHandler) HandleEvent(event *github.Event) {
 	h.LastEvent = event
 	h.EventWasHandled = true
+	h.Done <- true
 }
